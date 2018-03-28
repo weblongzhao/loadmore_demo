3509498a72f325437d95658c18e3cb6c9f348b60
Your Name <you@example.com>
2018-03-28 14:43:43 +0800

    loadmore

---
+++ server.js
@@ -0,0 +1,65 @@
+/*
+* @Author: Marte
+* @Date:   2018-03-28 09:04:59
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-03-28 13:56:01
+*/
+
+'use strict';
+var http = require("http")
+var fs = require("fs")
+var url = require("url")
+var path = require("path")
+
+http.createServer((request,response)=>{
+    response.setHeader("content-type","application/json;charset=utf-8")
+    var urlObj = url.parse(request.url,true) ;
+    console.log(urlObj)
+    if(urlObj.pathname == "/"){
+        var statusDir = __dirname+"/index.html";
+        fs.readFile(statusDir,function(err,data){
+            response.setHeader("content-type","text/html")
+            response.end(data)
+        })
+    } else if(urlObj.pathname == "/loadmore"){
+        var arr =[];
+        for(var i=0;i<5;i++){
+            arr.push(urlObj.query.index++);
+        }
+
+        response.end(JSON.stringify(arr))
+    } else{
+        var statusDir = path.join(__dirname,urlObj.pathname);
+        console.log(statusDir)
+        fs.readFile(statusDir,function(err,data){
+            response.setHeader("content-type","text/html")
+            response.end(data)
+        })
+    }
+
+}).listen("8888")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
